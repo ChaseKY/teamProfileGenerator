868baff706657d0593eb8a160ffe4f61e55ab4f3
Chase Young <chasekyoung@gmail.com>
2022-02-24 15:38:38 -0500

    added more functions

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateHTML = require('./generatehtml')
 const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
 const employees = [
 
 ]
@@ -91,8 +93,38 @@ function addEmployee() {
         message: 'Do you want to add another employee?'
     }).then((answer) => {
         console.log(answer);
-        
+        // ???
+        if (answer.anotherEmployee === true) {
+            addEorI()
+        }
     })
+}
+
+function addEorI() {
+    inquirer.prompt({
+        type: 'list',
+        name: 'EorI',
+        message: 'Do you want to add an engineer or intern?',
+        choices: ['Engineer', 'Intern']
+    }).then((answer) => {
+        console.log(answer);
+        if (answer.EorI === 'Engineer') {
+            inquirer.prompt(engineerInput)
+            .then((answers) => {
+                console.log(answers);
+                const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+                employees.push(newEngineer);
+        
+        })
+    }   else if (answer.EorI === 'Intern') {
+            inquirer.prompt(internInput)
+            .then((answers) => {
+                console.log(answers);
+                const newIntern = new Intern(answers.name, answers.id, answers.email, answers.school);
+                employees.push(newIntern);
+        })
+    }
+    }
 }
 
 init();
